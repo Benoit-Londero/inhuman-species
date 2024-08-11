1d71cefbda8e202aec5ddf3c4fd0bac8d8002bf3
Benito <you@example.com>
2024-08-11 20:31:07 +0200

    add megamnu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./scss/style.scss";
 import "./js/animate.js";
 import "./js/swiper.js";
+import "./js/megamenu.js";
 
 require("fslightbox");
---
+++ src/js/megamenu.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+     $('.open_menu').on('click',function(){
+          var sidebar = document.getElementById('megamenu');
+
+          sidebar.classList.toggle = 'open';
+     });
+});
