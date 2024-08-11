ea5c426316295bed01c264d8d133873e9c75eb63
Benito <you@example.com>
2024-08-11 20:32:08 +0200

    megamenu

--- header.php
+++ header.php
@@ -19,3 +19,4 @@
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
         <?php get_template_part('template-parts/sidebar');?>
+        <?php get_template_part('template-parts/megamenu');?>
