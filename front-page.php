327a18c1349478d7fc87e06f6dd77d2a150f2ded
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2026-06-27 08:10:59 +0200

    correction frontpage

--- front-page.php
+++ front-page.php
@@ -1,28 +1,10 @@
 <?php
-/* Template Name: Page d'accueil */
-
 get_header();
 
-$background   = get_field( 'background' );
-$background_2 = get_field( 'background_2' );
-?>
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+        the_content();
+    endwhile;
+endif;
 
-<div id="main-content">
-    <?php if ( $background ) : ?>
-        <img
-            src="<?php echo esc_url( $background['url'] ); ?>"
-            alt="<?php echo esc_attr( $background['alt'] ?? $background['name'] ?? '' ); ?>"
-            class="image-before"
-        />
-    <?php endif; ?>
-
-    <?php if ( $background_2 ) : ?>
-        <img
-            src="<?php echo esc_url( $background_2['url'] ); ?>"
-            alt="<?php echo esc_attr( $background_2['alt'] ?? $background_2['name'] ?? '' ); ?>"
-            class="image-after"
-        />
-    <?php endif; ?>
-</div>
-
-<?php get_footer(); ?>
+get_footer();
